0e8659053108c2c3cbd4bc199aa7ec9d462970b9
AbdealiJK <abdealikothari@gmail.com>
2019-04-24 16:54:34 +0530

    setup: Add basic package

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+version_ns = {}
+with open('pip_downloader/__init__.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            exec(line, version_ns)
+            break
+
+setup(
+    name='pip-downloader',
+    version=version_ns['__version__'],
+    py_modules=find_packages(include=['pip_downloader', 'pip_downloader.*']),
+    description='Download packages from pip for offline usage.',
+    long_description=open('README.md').read(),
+    license='MIT',
+    author='AbdealiJK',
+    author_email='abdealikothari@gmail.com',
+    url='https://github.com/AbdealiJK/pip-downloader',
+    install_requires=['click', 'requests'],
+    entry_points={
+        'console_scripts': ['pip-downloader = pip_downloader.cli:main'],
+    },
+)
